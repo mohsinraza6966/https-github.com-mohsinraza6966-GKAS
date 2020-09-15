f4f341c9454f40b8fdebe2c6a4b650285746da92
mohsin raza <mohsin_r_007@outlook.com>
2020-09-15 12:32:12 +0500

    minor bundling issues fixed

--- GKAS/App_Start/BundleConfig.cs
+++ GKAS/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace GKAS
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = true;
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
